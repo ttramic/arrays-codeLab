cb094d4f69b0b602d70ceac68d88a72a5bb5e1c1
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2023-02-07 15:10:26 +0100

    initial commit
    mangler lidt opgaver og praktiske eksempler

---
+++ assets/js/opgaver/arrayFunctions.js
@@ -0,0 +1,104 @@
+
+let myArray = ['element1', 'element2', 'element3'];
+let myPartArray = ['partElement1', 'partElement2', 'PartElement3'];
+
+
+
+
+//.........................................................
+// opgave 1 tilføj element4 til enden myArray
+
+// din kode her...
+console.log(myArray);
+
+// opgave 1 fjern element4 igen
+
+// din kode her...
+console.log(myArray);
+
+//.........................................................
+
+// opgave 2 tilføj element4 til starten myArray
+
+// din kode her...
+console.log(myArray);
+
+// opgave 2 fjern element4 igen
+
+// din kode her...
+console.log(myArray);
+
+//.........................................................
+
+let myFruits = ['æble', 'pære', 'banan', 'appelsin', 'citron'];
+let myVegetables = ['salat', 'blomkål', 'spidskål', 'agurk'];
+
+
+/* opgave 3 splice()
+ tilføj mandarin og fersken efter pære i myFruits
+ */
+
+// din kode her...
+
+myFruits.splice(2, 0, "mandarin", "fersken");
+
+console.log(myFruits);
+
+// opgave 3 fjern mandarin og fersken igen
+
+// din kode her...
+myFruits.splice(2, 2);
+
+console.log(myFruits);
+
+//.........................................................
+
+/* opgave 4 slice()
+træk blomkål og spidskål ud af arrayen myVegetables
+og ind i en ny array der hedder myCabbage*/
+
+// din kode her...
+
+
+
+console.log(myCabbage);
+
+//.........................................................
+/* opgave 5 concat()
+ sæt arrays myFruits og myVegetables sammen i en ny array der hedder myYummies
+ */
+
+
+// din kode her...
+
+
+
+console.log(myYummies);
+
+//.........................................................
+
+/* opgave 6 sort()
+ sorter array  myYummies fra opgave 5
+ */
+
+
+// din kode her...
+
+
+
+console.log(myYummies);
+
+/* opgave 6 reverse()
+ vend rækkefølgen på  array  myYummies fra opgave 6
+ */
+
+
+// din kode her...
+
+console.log(myYummies);
+
+//.........................................................
+
+
+
+
